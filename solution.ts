0307064652179fe56e6fa33cd983f4599b4b00d3
Murad07 <murad.pi22@gmail.com>
2025-11-16 23:42:25 +0600

    feat: Problem 5 solve

--- solution.ts
+++ solution.ts
@@ -48,3 +48,14 @@ type Item = {
 const filterByRating = (items: Item[]): Item[] => {
     return items.filter(item => item.rating >= 4);
 }
+
+
+type User = {
+    id: number;
+    name: string;
+    email: string;
+    isActive: boolean;
+}
+const filterActiveUsers = (users: User[]): User[] => {
+    return users.filter(user => user.isActive);
+}
